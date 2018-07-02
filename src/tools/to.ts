473fa8fb3f756d63036921c5fb84943a4db1ac93
Niklas Kühtmann <admin@i-zed.net>
2018-07-02 22:41:45 +0200

    clean up

--- src/tools/to.ts
+++ src/tools/to.ts
@@ -1,7 +1,7 @@
-import { Sink, MaybePromise, Operator, Pluck } from "..";
+import { Sink, MaybePromise, Operator, Pluck, Pipe } from "..";
 
 export type ConsumerFn<T> = (value: T) => MaybePromise<void>;
 
 export function to<T>(fn: ConsumerFn<T>): Operator<T, T, Pluck> {
-    return input => input.flush( Sink.to(fn) );
+    return (input: Pipe<T>) => input.flush( Sink.to(fn) );
 }
