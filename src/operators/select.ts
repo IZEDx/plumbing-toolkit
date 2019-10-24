c6e604326fa09af885270f8ee3267c7798ec1f25
Niklas Kühtmann <nk@ized.io>
2019-10-25 00:24:48 +0200

    🐛 Fix select return type

--- src/operators/select.ts
+++ src/operators/select.ts
@@ -2,15 +2,21 @@ import { through } from ".";
 import { Operator } from "../pipe-types";
 
 type TupleTypes<T> = T extends (infer K)[] ? K : never;
+export type Selection<
+    T extends {}, 
+    K1 extends keyof T, 
+    K2 extends (keyof T)|undefined , 
+    Kn extends (keyof T)[]
+> = K2 extends keyof T ? T[K1|K2|TupleTypes<Kn>] : T[K1];
 
 export function select<
     T extends {}, 
     K1 extends keyof T,
-    K2 extends (keyof T)|undefined,
-    Kn extends (keyof T)[],
-    R extends K2 extends keyof T ? T[K1|K2|TupleTypes<Kn>] : T[K1]
+    K2 extends (keyof T)|undefined = undefined,
+    Kn extends (keyof T)[] = [],
+    R extends Selection<T, K1, K2, Kn> = Selection<T, K1, K2, Kn>
 >
-(key1: K1, key2: K2, ...keys: Kn): Operator<T, R> {
+(key1: K1, key2?: K2, ...keys: Kn): Operator<T, R> {
     return through<T, R>( async (obj, outlet) => {
         if (key2 === undefined && keys.length === 0)
         {
