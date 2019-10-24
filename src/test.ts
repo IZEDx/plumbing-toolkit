6d9c5c6132123714b5474e059456f230658f9adf
Niklas Kühtmann <nk@ized.io>
2019-10-24 16:49:37 +0200

    random testing

--- src/test.ts
+++ src/test.ts
@@ -1,11 +1,18 @@
 import { pipe } from "./pipe";
-import { fibonacci } from "./springs";
+import { fibonacci, interval } from "./springs";
 import { context, forEach, filter, map, consume } from "./operators";
 
 pipe(fibonacci(20)).chain(
-    context(0),
+    context(-1),
     forEach(p => p.context++),
     filter(p => p.value % 2 == 0),
     map(p => `${p.context}: ${p.value}`),
+    consume(console.log)
+)
+
+pipe(interval(1000)).chain(
+    context([0, 1]),
+    forEach(p => p.context = [p.context[1], p.context[0] + p.context[1]]),
+    map(p => `${p.value}: ${p.context[0]}`),
     consume(console.log)
 )
