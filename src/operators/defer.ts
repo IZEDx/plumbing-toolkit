fda448a31507120b5331f11d2bbb8178a521e13c
Niklas Kühtmann <nk@ized.io>
2019-10-27 02:19:43 +0100

    ♻ Rename parallel to defer

--- src/operators/defer.ts
+++ src/operators/defer.ts
@@ -2,7 +2,7 @@ import { Operator } from "../pipe-types";
 import { through } from "./through";
 
 
-export function parallel<T>(): Operator<T, T>
+export function defer<T>(): Operator<T, T>
 {
     return through((v, outlet) => {
         outlet.next(v);
